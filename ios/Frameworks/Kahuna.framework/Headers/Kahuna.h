b78cb353d15c2e721c6e8001e334b1dac0cd8221
theawesomecoder <abhitheawesomecoder@gmail.com>
2018-10-19 16:43:36 +0530

    kahuna framework added

---
+++ ios/Frameworks/Kahuna.framework/Headers/Kahuna.h
@@ -0,0 +1,23 @@
+//
+//  Kahuna.h
+//  Kahuna
+//
+//  Created by Michael Selevan on 9/13/16.
+//  Copyright © 2016 Kahuna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kahuna.
+FOUNDATION_EXPORT double KahunaVersionNumber;
+
+//! Project version string for Kahuna.
+FOUNDATION_EXPORT const unsigned char KahunaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kahuna/PublicHeader.h>
+
+#import <Kahuna/KahunaAnalytics.h>
+#import <Kahuna/KAHNSObject+JSON.h>
+#import <Kahuna/KAHEvent.h>
+#import <Kahuna/KAHEventBuilder.h>
+#import <Kahuna/KahunaIncludes.h>
---
+++ ios/Frameworks/Kahuna.framework/Headers/KahunaIncludes.h
@@ -0,0 +1,17 @@
+//
+//  KahunaIncludes.h
+//  HowToUseKahuna
+//
+//  Created by Michael Selevan on 2/21/17.
+//  Copyright © 2017 Kahuna. All rights reserved.
+//
+
+#ifndef KahunaIncludes_h
+#define KahunaIncludes_h
+
+#ifdef __IPHONE_10_1
+#import <UserNotifications/UserNotifications.h>
+#endif
+
+
+#endif /* KahunaIncludes_h */
